9cc2a299188cc5cbf3c3379311852218ec7d5b17
krootjes <d.blokpoel@outlook.com>
2025-09-22 21:17:52 +0200

    fix: correct log message for clarity in port listening

--- main.go
+++ main.go
@@ -23,6 +23,6 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	log.Printf("Luisteren op: %s in de container…", port)
+	log.Printf("Luisteren op poort %s in de container…", port)
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
