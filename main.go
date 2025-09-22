760465aa4cac5a08c36b3b670afaf0566a25970c
krootjes <d.blokpoel@outlook.com>
2025-09-22 20:39:41 +0200

    chore: initial go web server

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Dit is een Go webserver. Hallo vanuit Go! 👋")
+	})
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	// Luisterpoort via env (handig voor Docker/K8s)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("listening on :%s …", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
